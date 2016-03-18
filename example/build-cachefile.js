eb6d1e25ab016fa6284c2257a0073e587917ea76
James Friend <james@jsdf.co>
2016-03-19 10:39:39 +1100

    fix linting errors and make code style rules mandatory

--- example/build-cachefile.js
+++ example/build-cachefile.js
@@ -1,37 +1,37 @@
-console.time('total')
-console.time('startup')
-var browserify = require('browserify')
-var browserifyCache = require('../')
-var fs = require('fs')
-var makeMetricsStream = require('./metrics')
-console.timeEnd('startup')
+console.time('total');
+console.time('startup');
+var browserify = require('browserify');
+var browserifyCache = require('../');
+var fs = require('fs');
+var makeMetricsStream = require('./metrics');
+console.timeEnd('startup');
 
-var cache = true
+var cache = true;
 
 var opts;
 if (cache) {
-  opts = {cacheFile: __dirname+'/output/cache.json'}
+  opts = {cacheFile: __dirname + '/output/cache.json'};
 } else {
-  opts = {}
+  opts = {};
 }
 
-console.time('cache fill')
-var b = browserify(browserifyCache.args)
-browserifyCache(b, opts)
-console.timeEnd('cache fill')
+console.time('cache fill');
+var b = browserify(browserifyCache.args);
+browserifyCache(b, opts);
+console.timeEnd('cache fill');
 
-b.on('changedDeps', function(updated) { console.log(['changed files:'].concat(updated||[]).join('\n')) })
-b.add(__dirname + '/test-module')
+b.on('changedDeps', function(updated) { console.log(['changed files:'].concat(updated || []).join('\n')); });
+b.add(__dirname + '/test-module');
 
-process.on('exit', function () { console.timeEnd('total') })
+process.on('exit', function() { console.timeEnd('total'); });
 
-run() // start test
+run(); // start test
 
 function run() {
-  console.time('bundle')
+  console.time('bundle');
   b.bundle()
-    .on('end', function(){ console.timeEnd('bundle') })
+    .on('end', function() { console.timeEnd('bundle'); })
     .pipe(makeMetricsStream())
-    .pipe(fs.createWriteStream(__dirname+'/output/bundle.js'))
+    .pipe(fs.createWriteStream(__dirname + '/output/bundle.js'));
 }
 
--- test/build.js
+++ test/build.js
@@ -1,98 +1,98 @@
-var test = require('tap').test
-var execFile = require('child_process').execFile
-var path = require('path')
-var through = require('through2')
-var fs = require('fs')
-var xtend = require('xtend')
-var rimraf = require('rimraf')
+var test = require('tap').test;
+var execFile = require('child_process').execFile;
+var path = require('path');
+var through = require('through2');
+var fs = require('fs');
+var xtend = require('xtend');
+var rimraf = require('rimraf');
 
-var basedir = path.resolve(__dirname, '../')
-var outputdir = path.join(basedir, 'example','output','test','build')
-var dynamicModule = path.join(outputdir, 'dynamic.js')
-var requiresDynamicModule = path.join(outputdir, 'requires-dynamic.js')
-var dependentFile = path.join(outputdir, 'dependent.txt')
+var basedir = path.resolve(__dirname, '../');
+var outputdir = path.join(basedir, 'example', 'output', 'test', 'build');
+var dynamicModule = path.join(outputdir, 'dynamic.js');
+var requiresDynamicModule = path.join(outputdir, 'requires-dynamic.js');
+var dependentFile = path.join(outputdir, 'dependent.txt');
 
-test("make sure it builds and builds again", function (t) {
-  t.plan(7)
+test('make sure it builds and builds again', function(t) {
+  t.plan(7);
 
-  rimraf(outputdir, {disableGlob:true}, function (err) {
-    t.notOk(err, 'dir removed')
-    execFile('mkdir', ['-p', outputdir], function (err) {
-      t.notOk(err, 'dir created')
-      fs.writeFileSync(requiresDynamicModule, 'require("./dynamic")')
-      build1()
-    })
-  })
+  rimraf(outputdir, {disableGlob:true}, function(err) {
+    t.notOk(err, 'dir removed');
+    execFile('mkdir', ['-p', outputdir], function(err) {
+      t.notOk(err, 'dir created');
+      fs.writeFileSync(requiresDynamicModule, 'require("./dynamic")');
+      build1();
+    });
+  });
 
-  function build1 () {
-    fs.writeFileSync(dynamicModule, 'console.log("a")')
+  function build1() {
+    fs.writeFileSync(dynamicModule, 'console.log("a")');
 
-    var b1 = make()
+    var b1 = make();
 
     b1.bundle()
       .pipe(through())
-      .on('finish', function () {
-        t.ok(true, 'built once')
+      .on('finish', function() {
+        t.ok(true, 'built once');
       })
-      .pipe(fs.createWriteStream(path.join(outputdir,'build1.js')))
-      .on('finish', function () {
-        setTimeout(function () {
-          build2()
-        }, 2000) // mtime resolution can be 1-2s depending on OS
-      })
+      .pipe(fs.createWriteStream(path.join(outputdir, 'build1.js')))
+      .on('finish', function() {
+        setTimeout(function() {
+          build2();
+        }, 2000); // mtime resolution can be 1-2s depending on OS
+      });
   }
 
-  function build2 () {
-    fs.writeFileSync(dynamicModule, 'console.log("b")')
+  function build2() {
+    fs.writeFileSync(dynamicModule, 'console.log("b")');
 
-    var b2 = make()
+    var b2 = make();
 
-    b2.on('changedDeps', function (invalidated) {
-      t.ok(invalidated && invalidated.length == 1, 'one file changed')
-    })
+    b2.on('changedDeps', function(invalidated) {
+      t.ok(invalidated && invalidated.length == 1, 'one file changed');
+    });
 
     b2.bundle()
       .pipe(through())
-      .on('finish', function () {
-        t.ok(true, 'built twice')
-        t.ok(Object.keys(b2._options.cache).length > 0, 'cache is populated')
+      .on('finish', function() {
+        t.ok(true, 'built twice');
+        t.ok(Object.keys(b2._options.cache).length > 0, 'cache is populated');
       })
-      .pipe(fs.createWriteStream(path.join(outputdir,'build2.js')))
-      .on('finish', function () {
-        setTimeout(function () {
-          build3()
-        }, 2000) // mtime resolution can be 1-2s depending on OS
-      })
+      .pipe(fs.createWriteStream(path.join(outputdir, 'build2.js')))
+      .on('finish', function() {
+        setTimeout(function() {
+          build3();
+        }, 2000); // mtime resolution can be 1-2s depending on OS
+      });
   }
 
-  function build3 () {
+  function build3() {
     // dependentFile is changed
-    fs.writeFileSync(dependentFile, 'hello')
+    fs.writeFileSync(dependentFile, 'hello');
 
-    var b3 = make()
+    var b3 = make();
 
     // TODO Not sure how to assert that dynamicModule was invalidated
 
     b3.bundle()
       .pipe(through())
-      .on('finish', function () {
-        t.ok(true, 'built thrice')
-        t.end()
+      .on('finish', function() {
+        t.ok(true, 'built thrice');
+        t.end();
       })
-      .pipe(fs.createWriteStream(path.join(outputdir,'build3.js')))
+      .pipe(fs.createWriteStream(path.join(outputdir, 'build3.js')));
   }
-})
+});
 
-function make () {
-  var browserify = require('browserify')
-  var browserifyCache = require('../')
+function make() {
+  var browserify = require('browserify');
+  var browserifyCache = require('../');
 
-  var opts = xtend({cacheFile: path.join(outputdir,'cache.json')}, browserifyCache.args)
+  var opts = xtend({cacheFile: path.join(outputdir, 'cache.json')}, browserifyCache.args);
 
-  var b = browserify(opts)
-  browserifyCache(b)
+  var b = browserify(opts);
+  browserifyCache(b);
 
-  b.add(requiresDynamicModule)
+  b.add(requiresDynamicModule);
 
   // Simulate a transform that includes "dependent.txt" in "dynamic.js"
   b.transform(function(file) {
@@ -104,7 +104,7 @@ function make () {
       this.emit('file', dependentFile);
       cb();
     });
-  })
+  });
 
-  return b
+  return b;
 }
